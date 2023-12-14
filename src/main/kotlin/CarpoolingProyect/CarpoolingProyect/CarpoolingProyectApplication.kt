7307ef85f502a2dae1be50ab8abd56a9e2fa19f2
Miguell Criollo <macriollo.2@sudamericano.edu.ec>
2023-12-14 13:13:19 -0500

    Initializer Files

---
+++ src/main/kotlin/CarpoolingProyect/CarpoolingProyect/CarpoolingProyectApplication.kt
@@ -0,0 +1,11 @@
+package CarpoolingProyect.CarpoolingProyect
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class CarpoolingProyectApplication
+
+fun main(args: Array<String>) {
+	runApplication<CarpoolingProyectApplication>(*args)
+}
