4becae3eba4fee8be53bbfd56e2b8276b0b4f635
davidpozoio <117756174+davidpozoio@users.noreply.github.com>
2023-12-18 09:16:12 -0500

    id from stop correction

--- src/main/kotlin/CarpoolingProyect/CarpoolingProyect/Model/Stop.kt
+++ src/main/kotlin/CarpoolingProyect/CarpoolingProyect/Model/Stop.kt
@@ -1,16 +1,19 @@
 package CarpoolingProyect.CarpoolingProyect.Model
 
+import jakarta.persistence.Column
 import jakarta.persistence.Entity
 import jakarta.persistence.GeneratedValue
 import jakarta.persistence.GenerationType
+import jakarta.persistence.Id
 import jakarta.persistence.Table
-import org.springframework.data.annotation.Id
+
 
 @Entity
 @Table(name = "stop")
 class Stop {
     @GeneratedValue(strategy = GenerationType.IDENTITY)
-    @Id()
+    @Id
+    @Column(updatable = false)
     var id: Long? = null
 
     var name: String? = null
