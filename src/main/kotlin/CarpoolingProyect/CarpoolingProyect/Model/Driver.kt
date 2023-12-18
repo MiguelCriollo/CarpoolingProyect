c1835d03d5ac3257ba7f4370177e9c25db57dec9
Miguell Criollo <macriollo.2@sudamericano.edu.ec>
2023-12-18 07:59:09 -0500

    Driver Model

---
+++ src/main/kotlin/CarpoolingProyect/CarpoolingProyect/Model/Driver.kt
@@ -0,0 +1,19 @@
+package CarpoolingProyect.CarpoolingProyect.Model
+
+import jakarta.persistence.*
+import jakarta.validation.constraints.Email
+import jakarta.validation.constraints.NotBlank
+@Entity
+@Table(name = "driver")
+class Driver {
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Id
+    @Column(updatable = false)
+    var id: Long? = null
+    @NotBlank(message="Campo obligatorio") //validate
+    @Column(name="driver_licence")
+    var driverLicence: String? = null
+    @NotBlank(message="Campo obligatorio") //validate
+    @Column(name="user_id")
+    var userId: String?= null
+}
