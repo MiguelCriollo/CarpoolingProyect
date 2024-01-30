fdf05cc84a5da669a53d62fabae0b4f76b93a0ab
Miguell Criollo <macriollo.2@sudamericano.edu.ec>
2024-01-30 09:54:10 -0500

    Xd

--- src/main/kotlin/CarpoolingProyect/CarpoolingProyect/Exceptions/ExceptionsResponses.kt
+++ src/main/kotlin/CarpoolingProyect/CarpoolingProyect/Exceptions/ExceptionsResponses.kt
@@ -4,6 +4,7 @@ import org.springframework.http.HttpStatus
 import org.springframework.web.bind.annotation.ResponseStatus
 import java.io.Serializable
 import java.lang.RuntimeException
+open class BasisErrorException(message: String) : RuntimeException(message)
 
 @ResponseStatus(HttpStatus.NOT_FOUND)
 data class NotFoundResponse(
@@ -18,3 +19,4 @@ data class ErrorResponse(
     val error: String="",
     val message: String=""
 )
+
